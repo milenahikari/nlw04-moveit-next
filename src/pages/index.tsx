631f5c369cc1c7811492268e624280d6949a5d10
Milena <milenahikari11@gmail.com>
2021-02-27 14:31:07 -0300

    Adicionando title

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -3,11 +3,16 @@ import { Profile } from '../components/Profile';
 import { CompletedChallenges } from '../components/CompletedChallenges';
 import { Countdown } from '../components/Countdown';
 
+import { Head } from 'next/document';
+
 import styles from '../styles/pages/Home.module.css';
 
 export default function Home() {
   return (
     <div className={styles.container}>
+      <Head>
+        <title>Início | move.it</title>
+      </Head>
       <ExperienceBar />
 
       <section>
