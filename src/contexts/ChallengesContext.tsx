6a29fc3772133abaf81548060aeed655b48730f2
Milena <milenahikari11@gmail.com>
2021-02-27 17:22:20 -0300

    Apresentando um novo desafio

--- src/contexts/ChallengesContext.tsx
+++ src/contexts/ChallengesContext.tsx
@@ -1,9 +1,17 @@
 import { createContext, useState, ReactNode } from 'react';
+import challenges from "../../challenges.json";
+
+interface Challenge {
+  type: 'body' | 'eye';
+  description: string;
+  amount: number;
+}
 
 interface ChallengesContextData {
   level: number;
   currentExperience: number;
   challengesCompleted: number;
+  activeChallenge: Challenge;
   levelUp: () => void;
   startNewChallenge: () => void;
 }
@@ -18,13 +26,16 @@ export function ChallengesProvider({ children }: ChallengesProviderProps) {
   const [level, setLevel] = useState(1);
   const [currentExperience, setCurrentExperience] = useState(0);
   const [challengesCompleted, setChallengesCompleted] = useState(0);
+  const [activeChallenge, setActiveChallege] = useState(null);
 
   function levelUp() {
     setLevel(level + 1);
   }
 
   function startNewChallenge() {
-    console.log('New challenge');
+    const randomChallengeIndex = Math.floor(Math.random() * challenges.length);
+    const challenge = challenges[randomChallengeIndex];
+    setActiveChallege(challenge);
   }
 
   return (
@@ -33,8 +44,9 @@ export function ChallengesProvider({ children }: ChallengesProviderProps) {
         level,
         currentExperience,
         challengesCompleted,
+        activeChallenge,
         levelUp,
-        startNewChallenge
+        startNewChallenge,
       }}
     >
       {children}
