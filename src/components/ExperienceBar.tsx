17faccccfb61388eb056f6607d109323191752aa
Milena <milenahikari11@gmail.com>
2021-02-28 18:29:56 -0300

    Tornado o layout responsivo

--- src/components/ExperienceBar.tsx
+++ src/components/ExperienceBar.tsx
@@ -6,7 +6,6 @@ import styles from "../styles/components/ExperienceBar.module.css";
 export function ExperienceBar() {
   const { currentExperience, experienceToNextLevel } = useContext(ChallengesContext);
 
-  console.log(currentExperience);
   const percentToNextLevel = Math.round(currentExperience * 100) / experienceToNextLevel;
 
   return (
